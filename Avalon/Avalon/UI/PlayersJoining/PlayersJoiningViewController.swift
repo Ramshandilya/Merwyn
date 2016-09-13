974fdd7561e449b2b0815f85d675c3c3f8dbf078
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2016-09-13 10:47:53 +0530

    A little housekeeping

---
+++ Avalon/Avalon/UI/PlayersJoining/PlayersJoiningViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlayersJoiningViewController.swift
+//  Avalon
+//
+//  Created by Ramsundar Shandilya on 9/12/16.
+//  Copyright © 2016 Merwyn Games. All rights reserved.
+//
+
+import UIKit
+
+class PlayersJoiningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
